8db6314bc82a8cb37bfcf0e39a16b207be4dceb1
Marco Mariani <marco@crowdsec.net>
2021-11-17 16:22:25 +0100

    cs-specific backend and environment

---
+++ environment.sh
@@ -0,0 +1,6 @@
+
+export TF_VAR_aws_region="$AWS_REGION"
+export TF_VAR_aws_access_key_id="$AWS_ACCESS_KEY_ID"
+export TF_VAR_aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
+export TF_VAR_aws_session_token="$AWS_SESSION_TOKEN"
+
